16422786d7ce348a9322753ff0037ec93faf0193
Wolverine <wolverine.karbon14@gmail.com>
2018-07-31 00:15:36 -0300

    Initial commit :rocket:

---
+++ src/whitepaper.js
@@ -0,0 +1,37 @@
+export const whitepaper = {
+  words: {
+    index: {
+      ES: 'Indice',
+      EN: 'Index',
+    },
+  },
+  resume: {
+    ES: `Karbon14 es la primera aplicación en utilizar los beneficios de la blockchain para realizar certificados de 
+pruebas de vida. Los datos que se utilizarán para crear los certificados de pruebas de vida generan un hash único que 
+será guardado en un smart contact. Para mayor seguridad y protección de los datos personales Karbon14 no almacenará y 
+no guardará ningún dato del usuario. El objetivo de Karbon14 es brindar la posibilidad de crear certificados de pruebas
+de vida de una manera rápida y sencilla. Un certificado de prueba de vida para Karbon14 consta de información brindada 
+por el usuario, aprobada por las crypto escribanías de Karbon14 y persistiendo en la blockchain. Karbon14 tiene como 
+visión desarrollar una comunidad descentralizada creando una nueva utilidad en la blockchain. Explorando de esta manera 
+una novedosa forma de brindar un servicio nuevo para la realización de certificados de pruebas de vida. El certificado 
+de prueba de vida es un documento inmutable, por lo cual no puede verse alterado. Es decir que puede ser utilizados por 
+entidades que requieran pruebas de vida para empezar a operar, o para instituciones que exijan cada determinada cantidad 
+de tiempo la renovación de esas pruebas de vida. Uno de los principales objetivos de Karbon14 es que los usuarios acorten 
+los tiempos de espera, adquieran un servicio confiable y fácil de utilizar, con la tranquilidad de que sus certificados 
+estarán seguros, absolutamente inmutables, con alta disponibilidad y que perdurarán en el tiempo. Las potencialidades de 
+Karbon14 no tiene límites, ya que es un territorio sin explorar y sin actores en el mercado aún. `,
+    EN: `Karbon14 is the first application to use the benefits of the blockchain to carry out life test certificates. 
+The data that will be used to create the life test certificates generates a unique hash that will be saved in a smart 
+contact. For greater security and protection of personal data Karbon14 will not store and save any data of the user. The 
+objective of Karbon14 is to offer the possibility of creating life test certificates in a fast and simple way. A proof 
+of life certificate for Karbon14 consists of information provided by the user, approved by the crypto desk of Karbon14
+and persisting in the blockchain. Karbon14 has the vision of developing a decentralized community by creating a new 
+utility in the blockchain. Exploring in this way a novel way to provide a new service for the realization of life test 
+certificates. The proof of life certificate is an immutable document, so it can not be altered. That is to say that they 
+can be used by entities that require life tests to start operating, or for institutions that demand a certain amount of 
+time the renewal of those life tests. One of the main objectives of Karbon14 is that users shorten waiting times, acquire 
+a reliable and easy to use service, with the peace of mind that their certificates will be secure, absolutely immutable, 
+with high availability and that will last over time. The potential of Karbon14 has no limits, since it is a territory 
+without exploring and without actors in the market yet.`,
+  },
+}
