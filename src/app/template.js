767f48a93d7a570fe9f78ed96ad23329b1fb93d5
Wolverine <wolverine.karbon14@gmail.com>
2018-08-10 16:21:17 -0300

    styles

--- src/app/template.js
+++ src/app/template.js
@@ -5,7 +5,11 @@ export default ({ body, title }) => {
       <head>
         <title>${title}</title>
       </head>
-      
+      <style>
+        body {
+          background-color: #4C4B4E
+        }
+      </style>
       <body>
         <div id="root">${body}</div>
       </body>
--- src/whitepaper/styles.js
+++ src/whitepaper/styles.js
@@ -2,13 +2,25 @@ import { Font, StyleSheet } from '@react-pdf/react-pdf'
 
 export const styles = StyleSheet.create({
   title: {
+    display: 'block',
     fontSize: 24,
     textAlign: 'center',
   },
-  body: {
-    paddingTop: 35,
-    paddingBottom: 45,
-    paddingHorizontal: 35,
+  document: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '0 auto',
+    width: 794,
+  },
+  page: {
+    padding: '35px 65px',
+    boxSizing: 'border-box',
+    backgroundColor: '#FFF',
+    width: 794,
+    height: 1122,
+    marginBottom: 20,
+    boxShadow: '0px 5px 10px #000',
+    position: 'relative',
   },
   text: {
     margin: 12,
@@ -16,14 +28,17 @@ export const styles = StyleSheet.create({
     textAlign: 'justify',
   },
   header: {
+    display: 'block',
     fontSize: 12,
     color: 'grey',
-    marginBottom: 15,
+    marginBottom: '40px',
     textAlign: 'center',
+    paddingTop: '10px',
   },
   footer: {
+    display: 'block',
     position: 'absolute',
-    fontSize: 12,
+    fontSize: '12pt',
     bottom: 25,
     left: 35,
     right: 0,
