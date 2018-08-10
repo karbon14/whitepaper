dd5117fe02c57eb3a8d8a5ccccdacd9728d2ef86
zalazar natanael <zalazar.natanael@gmail.com>
2018-08-10 19:09:30 -0300

    Add route with params lang

--- src/app/server.js
+++ src/app/server.js
@@ -2,6 +2,7 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import serve from 'koa-static'
 import React from 'react'
+import bodyParser from 'koa-bodyparser'
 import { renderToString } from 'react-dom/server'
 import { Karbon14Whitepaper } from '../whitepaper/whitepaper'
 import template from './template'
@@ -10,8 +11,13 @@ const PORT = process.env.PORT || 3000
 const app = new Koa()
 const router = new Router()
 
+const renderWhitepaper = (lang = 'EN') => {
+  const langUpper = lang.toUpperCase()
+  return renderToString(<Karbon14Whitepaper lang={langUpper} />)
+}
+
 router.get('/', (ctx) => {
-  const appString = renderToString(<Karbon14Whitepaper />)
+  const appString = renderWhitepaper()
 
   ctx.body = template({
     body: appString,
@@ -19,8 +25,20 @@ router.get('/', (ctx) => {
   })
 })
 
+router.get('/:lang', (ctx) => {
+  const { lang } = ctx.params
+  const appString = renderWhitepaper(lang)
+
+  ctx.body = template({
+    body: appString,
+    title: 'Karbon14 - Whitepaper',
+  })
+})
+
+
 app
   .use(serve(__dirname + '/public'))
+  .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods())
 
