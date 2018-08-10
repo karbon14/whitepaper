88dca4229e336682c78a310e67133fbc9b416e38
Wolverine <wolverine.karbon14@gmail.com>
2018-08-10 15:06:49 -0300

    development mode

---
+++ browser-sync.js
@@ -0,0 +1,7 @@
+module.exports = {
+  proxy: 'localhost:3000',
+  reloadDelay: 1500,
+  browser: 'google chrome',
+  port: 4000,
+  files: ['src/whitepaper/sections'],
+}
---
+++ src/app/template.js
@@ -0,0 +1,15 @@
+export default ({ body, title }) => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${title}</title>
+        <link rel="stylesheet" href="/assets/index.css" />
+      </head>
+      
+      <body>
+        <div id="root">${body}</div>
+      </body>
+    </html>
+  `
+}
