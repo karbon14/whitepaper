f0386b2eced117e99b2cba239ff0a3c0a30a0cfc
Wolverine <wolverine.karbon14@gmail.com>
2018-08-10 17:40:39 -0300

    Development Mode (#7)

--- src/whitepaper/styles.js
+++ src/whitepaper/styles.js
@@ -1,29 +1,48 @@
 import { Font, StyleSheet } from '@react-pdf/react-pdf'
 
+const isProduction = process.env.NODE_ENV === 'production'
+const fontUnit = (size) => isProduction ? size : `${size}pt`
+
 export const styles = StyleSheet.create({
   title: {
-    fontSize: 24,
+    display: 'block',
+    fontSize: fontUnit(24),
     textAlign: 'center',
   },
-  body: {
-    paddingTop: 35,
-    paddingBottom: 45,
-    paddingHorizontal: 35,
+  document: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '0 auto',
+    width: 794,
+  },
+  page: {
+    paddingTop: fontUnit(35),
+    paddingLeft: fontUnit(50),
+    paddingRight: fontUnit(50),
+    boxSizing: 'border-box',
+    backgroundColor: '#FFF',
+    width: 794,
+    height: 1122,
+    marginBottom: fontUnit(13),
+    boxShadow: '0px 5px 10px #000',
+    position: 'relative',
   },
   text: {
-    margin: 12,
-    fontSize: 12,
+    margin: fontUnit(12),
+    fontSize: fontUnit(12),
     textAlign: 'justify',
   },
-  header: {
-    fontSize: 12,
+  header: {...{
+    display: 'block',
+    fontSize: fontUnit(12),
     color: 'grey',
-    marginBottom: 15,
     textAlign: 'center',
-  },
+    paddingTop: fontUnit(10),
+  }, ...isProduction ? {} : { marginBottom: fontUnit(16) } },
   footer: {
+    display: 'block',
     position: 'absolute',
-    fontSize: 12,
+    fontSize: fontUnit(12),
     bottom: 25,
     left: 35,
     right: 0,
--- src/whitepaper/whitepaper.js
+++ src/whitepaper/whitepaper.js
@@ -9,14 +9,14 @@ const index = yimp.read(path.join(__dirname, 'sections/index.yml'))
 const resume = yimp.read(path.join(__dirname, 'sections/resume.yml'))
 
 export const Karbon14Whitepaper = ({ lang }) => (
-  <Document>
-    <Page size="A4" style={styles.body}>
+  <Document style={styles.document}>
+    <Page size="A4" style={styles.page}>
       <Text style={styles.title}>Karbon14</Text>
     </Page>
-    <Page size="A4" style={styles.body}>
+    <Page size="A4" style={styles.page}>
       <Text style={styles.title}>{index[lang].TITLE}</Text>
     </Page>
-    <Page size="A4" style={styles.body}>
+    <Page size="A4" style={styles.page}>
       <Text style={styles.header} fixed>
         ~ Karbon14 ~
       </Text>
